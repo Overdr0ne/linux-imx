da6f373725831518ed2153c828e2e65598b706a8
Samuel Morris <scmorris.dev@gmail.com>
2025-12-04 13:10:55 -0800

    Revert "MT-134856: Silence noisy console messages (#29)"
    This reverts commit bbe0e38da1c6300bd61184ef2709ce506b25da9a.

--- drivers/usb/chipidea/usbmisc_imx.c
+++ drivers/usb/chipidea/usbmisc_imx.c
@@ -871,9 +871,7 @@ static int imx7d_charger_detection(struct imx_usbmisc_data *data)
 	/* Check if vbus is valid */
 	val = readl(usbmisc->base + MX7D_USB_OTG_PHY_STATUS);
 	if (!(val & MX7D_USB_OTG_PHY_STATUS_VBUS_VLD)) {
-#ifdef MT_DEBUG
 		dev_err(data->dev, "vbus is error\n");
-#endif		
 		return -EINVAL;
 	}
 
@@ -1243,11 +1241,9 @@ int imx_usbmisc_charger_detection(struct imx_usbmisc_data *data, bool connect)
 	if (connect) {
 		ret = usbmisc->ops->charger_detection(data);
 		if (ret) {
-#ifdef MT_DEBUG
 			dev_err(data->dev,
 					"Error occurs during detection: %d\n",
 					ret);
-#endif
 			usb_phy->chg_state = USB_CHARGER_ABSENT;
 		} else {
 			usb_phy->chg_state = USB_CHARGER_PRESENT;
